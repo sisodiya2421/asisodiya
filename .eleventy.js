71b3690fcd29a0ae76cc2041ac2670e051c555ab
Abhishek Sisodiya <sisodiya2421@gmail.com>
2020-11-12 23:15:58 +0530

    minifying html

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,6 @@ const { __esModule } = require("tailwindcss/lib/util/createPlugin");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('assets/icons');
+    // minify HTML
+    eleventyConfig.addTransform('htmlminify', require('./lib/transforms/htmlminify'));
 }
